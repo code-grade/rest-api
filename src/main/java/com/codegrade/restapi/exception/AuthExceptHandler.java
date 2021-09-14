b8f717c4698563ecd34632a07cce849fad37c4d5
Thilina Lakshan <thilinalakshanmail@gmail.com>
2021-09-14 14:51:48 +0530

    handle account disabled exception

--- src/main/java/com/codegrade/restapi/exception/AuthExceptHandler.java
+++ src/main/java/com/codegrade/restapi/exception/AuthExceptHandler.java
@@ -4,12 +4,12 @@ import com.codegrade.restapi.utils.RBuilder;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -20,33 +20,33 @@ import java.io.OutputStream;
 public class AuthExceptHandler implements AuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request,
-                                        HttpServletResponse httpServletResponse,
+                                        HttpServletResponse response,
                                         AuthenticationException ex) throws IOException {
 
-        OutputStream out = httpServletResponse.getOutputStream();
+        OutputStream out = response.getOutputStream();
         ObjectMapper mapper = new ObjectMapper();
 
         if (BadCredentialsException.class.equals(ex.getClass())) {
             mapper.writerWithDefaultPrettyPrinter().writeValue(out,
                     RBuilder.badRequest().setMsg("Invalid username or password").compact()
             );
-            httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        } else if(DisabledException.class.equals(ex.getClass())) {
+            mapper.writerWithDefaultPrettyPrinter().writeValue(out,
+                    RBuilder.badRequest().setMsg("account is disabled").compact()
+            );
         } else if (InternalAuthenticationServiceException.class.equals(ex.getClass())) {
             mapper.writerWithDefaultPrettyPrinter().writeValue(out,
                     RBuilder.badRequest().setMsg("account is disabled or email is not verified").compact()
             );
-            httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         } else {
             mapper.writerWithDefaultPrettyPrinter().writeValue(out,
                     RBuilder.error().setData("error", ex.toString()).compact()
             );
-            log.info(ex.toString());
+            log.info(ex.getMessage());
             ex.printStackTrace();
-            httpServletResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
-
-
-        out.flush();
     }
 }
 
