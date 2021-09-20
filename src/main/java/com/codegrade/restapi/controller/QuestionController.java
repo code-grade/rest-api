387ecf2a628a2f00df0aed1c8b3a22f2dbdd22e6
Thilina Lakshan <thilinalakshanmail@gmail.com>
2021-09-20 14:46:49 +0530

    add question controller

---
+++ src/main/java/com/codegrade/restapi/controller/QuestionController.java
@@ -0,0 +1,15 @@
+package com.codegrade.restapi.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RestController;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@RestController
+@Validated
+public class QuestionController {
+}
